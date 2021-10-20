f68138dc75129550bcdca9048eeead686b27e62e
Nikolay Kolev <n.kolew@gmail.com>
2021-10-20 13:25:06 +0300

    playing with views and urls

--- events/models.py
+++ events/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.db.models.fields.related import ForeignKey
 
-# Create your models here.
+
+class Venue(models.Model):
+    name = models.CharField('Venue name', max_length=120)
+    address = models.CharField(max_length=300)
+    zip_code = models.CharField('Zip/Post Code', max_length=12)
+    phone = models.CharField('Contact Phone', max_length=20)
+    web = models.URLField('Web Address')
+    email_address = models.EmailField('Email Address')
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class MyClubUser(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField('User Email')
+
+    def __str__(self) -> str:
+        return self.first_name + ' ' + self.last_name
+
+
+class Event(models.Model):
+    name = models.CharField('Event Name', max_length=120)
+    event_date = models.DateTimeField('Event Date')
+    venue = ForeignKey(Venue, blank=True, null=True, on_delete=models.CASCADE)
+    manager = models.CharField(max_length=60)
+    attendees = models.ManyToManyField(MyClubUser, blank=True)
+    description = models.TextField(blank=True)
+
+    def __str__(self) -> str:
+        return self.name
--- events/views.py
+++ events/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from datetime import date
+import calendar
+from calendar import HTMLCalendar
 
-# Create your views here.
+
+def index(request, year=date.today().year, month=date.today().month):
+    year = int(year)
+    month = int(month)
+    if not (2000 <= year <= 2099):
+        year = date.today().year
+    month_name = calendar.month_name[month]
+    title = f"MyClub Event Calendar - {month_name} {year}"
+    cal = HTMLCalendar().formatmonth(year, month)
+    return HttpResponse(f"<h1>{title}</h1><p>{cal}</p>")
