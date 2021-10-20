d00fa76468c43acbe47654786b9e153ec492da18
Nikolay Kolev <n.kolew@gmail.com>
2021-10-20 13:40:40 +0300

    set SECRET_KEY as environment variable

--- myclub_site/settings.py
+++ myclub_site/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-po##+*%p^+$e0c%qr^z!siu%4ow_sqmh80$&6-2d29@l8i@&0z'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
