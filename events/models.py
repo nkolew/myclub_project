81780ba515a0507625ce2d8cd004f51757297e90
Nikolay Kolev <n.kolew@gmail.com>
2021-10-21 16:26:47 +0300

    end of free content

--- events/models.py
+++ events/models.py
@@ -2,6 +2,11 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
+class VenueManager(models.Manager):
+    def get_queryset(self):
+        return super(VenueManager, self).get_queryset().filter(zip_code='00000')
+
+
 class Venue(models.Model):
     name = models.CharField('Venue name', max_length=120)
     address = models.CharField(max_length=300)
@@ -9,6 +14,8 @@ class Venue(models.Model):
     phone = models.CharField('Contact Phone', max_length=20, blank=True)
     web = models.URLField('Web Address', blank=True)
     email_address = models.EmailField('Email Address', blank=True)
+    venues = models.Manager()
+    local_venues = VenueManager()
 
     def __str__(self) -> str:
         return self.name
@@ -23,6 +30,11 @@ class MyClubUser(models.Model):
         return self.first_name + ' ' + self.last_name
 
 
+class EventManager(models.Manager):
+    def event_type_count(self, event_type):
+        return self.filter(name__icontains=event_type).count()
+
+
 class Event(models.Model):
     name = models.CharField('Event Name', max_length=120)
     event_date = models.DateTimeField('Event Date')
@@ -32,6 +44,24 @@ class Event(models.Model):
         User, blank=True, null=True, on_delete=models.SET_NULL)
     attendees = models.ManyToManyField(MyClubUser, blank=True)
     description = models.TextField(blank=True)
+    events = EventManager()
+
+    def save(self, *args, **kwargs):
+        self.manager = User.objects.get(
+            username='admin')  # User 'admin' must exist
+        super(Event, self).save(*args, **kwargs)
+
+    def event_timing(self, date):
+        if self.event_date > date:
+            return "Event is after this date"
+        elif self.event_date == date:
+            return "Event is on the same day"
+        else:
+            return "Event is before this date"
+
+    @property
+    def name_slug(self):
+        return self.name.lower().replace(' ', '-')
 
     def __str__(self) -> str:
         return self.name
